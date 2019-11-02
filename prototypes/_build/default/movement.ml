e37d3bd02d822bfa5985af5c8bc93d3203ec0163
Jessica Lu <jessica@traceroute.us>
2019-11-02 16:07:11 -0400

    Edited movement.ml
    - enemy can be drawn now

--- prototypes/_build/default/movement.ml
+++ prototypes/_build/default/movement.ml
@@ -25,21 +25,22 @@ let update_pos s =
     | 'q' -> exit 0
     | _ -> ()
 
-let rec game_loop s = 
+let rec game_loop s e = 
   (* sleepf 0.005; *)
   update_pos s;
   clear_graph ();
   draw_image s.img s.x s.y;
-  game_loop s
+  draw_image e.img e.x e.y;
+  game_loop s e
 
 let init s = draw_image s.img s.x s.y
 
 let main () = 
   open_graph " 640x480";
   let player = { speed = 8; img = create_image 50 50; x = 320; y = 240; } in 
-  let enemy = { img = create_image 30 30; speed = 5; x = 500; y = 900} in
+  let enemy = { speed = 5; img = create_image 50 50; x = 100; y = 100; } in
   init player;
   init enemy;
-  game_loop player
+  game_loop player enemy
 
 let () = main ()
