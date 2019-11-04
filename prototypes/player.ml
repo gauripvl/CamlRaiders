e6231e121e1fc6a93a30b5bcc736bd793281f427
Pinxuan Huang <pinxhuang@gmail.com>
2019-11-04 09:57:22 -0500

    Working on player module

--- prototypes/player.ml
+++ prototypes/player.ml
@@ -1,1 +1,45 @@
 (* TODO *)
+
+(* module type Player = sig
+   type t
+   val get_speed : t -> int
+   val get_x : t -> int
+   val get_y : t -> int
+   val img_to_transp : Graphics.image -> Graphics.image
+   val draw : t -> unit
+   val update_pos : t -> unit
+   end *)
+
+module Player = 
+struct 
+  (** AF: TODO
+      RI: TODO *)
+  type t = {
+    img: Graphics.image;
+    mutable lives: int;
+    speed: int;
+    mutable x: int;
+    mutable y: int;
+  }
+
+  let get_speed t =
+    t.speed
+
+  let get_x t =
+    t.x
+
+  let get_y t =
+    t.y 
+
+  let draw t = Graphics.draw_image t.img t.x t.y
+
+  let update_pos t = 
+    if Graphics.key_pressed () then 
+      match Graphics.read_key () with 
+      | 'w' -> t.y <- t.y + t.speed
+      | 'a' -> t.x <- t.x - t.speed
+      | 's' -> t.y <- t.y - t.speed
+      | 'd' -> t.x <- t.x + t.speed
+      | 'q' -> exit 0
+      | _ -> ()
+end
--- prototypes/player.mli
+++ prototypes/player.mli
@@ -1,33 +1,31 @@
-(* TODO *)
-
-open Sprite
-
-(** The type of elements. *)
 module type Player = sig
-  type t
-  include Sprite.t with type t := t
-end
 
-(** A [Set] contains elements, which must be comparable. *)
-module type Set = sig
-
-  (** [Elt] is a module representing the type of elements
-      in the set and functions on them. *)
-  module Elt : ElementSig
-
-  (** [elt] is the type of elements in the set. *)
-  type elt = Elt.t
-
-  (** [t] is the type of sets. *)
   type t
 
+  (* ================= COMMON SPRITE FIELDS ================= *)
+
+  (** [get_speed t] is the speed of [t] *)
+  val get_speed : t -> int
+
+  (** [get_x t] is the x-coordinate of [t] *)
+  val get_x : t -> int
+
+  (** [get_y t] is the y-coordinate of [t] *)
+  val get_y : t -> int
+
+  (** [img_to_transp i] is [i] with black color 
+      changed to [transp] *)
+  val img_to_transp : Graphics.image -> Graphics.image
+
+  (** [draw t] draws the image of [s] onto the GUI. *)
+  val draw : t -> unit
+
+  (* ================= COMMON SPRITE FIELDS (END) ================= *)
+
+  (** [update_pos t] updates the position of the ship based on key presses. 
+      Pressing 'w'moves the ship up, 'a' moves the ship left, 
+      'd' moves the ship right, and 's' moves the ship down. 
+      Pressing 'q' quits the game and any other key does nothing. *)
+  val update_pos : t -> unit
+
 end
-
-(** [MakeSetOfDictionary] implements a set as a dictionary.
-    The keys of the dictionary represent the elements of the set.  
-    The values of the dictionary are irrelevant. *)
-module Make :   
-  functor (E : ElementSig)
-    -> functor (DM : DictionaryMaker)
-    -> Set with module Elt = E
-
