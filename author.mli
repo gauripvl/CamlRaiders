cee5e3a2ddca14ba0e1a7275646bd2a678334420
Pinxuan Huang <pinxhuang@gmail.com>
2019-11-04 15:18:02 -0500

    Fix authors

--- author.mli
+++ author.mli
@@ -1,7 +1,8 @@
 (**
    Metadata about this submission.
-   @author Full Name (netID). 
+   @author Full Name (netID)
 *)
 
-(** [hours_worked] is the number of hours you worked on this assignment. *)
-val hours_worked : int
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
