cd37fc944e8e98ec3b62ef2d087e741394898afd
Pinxuan Huang <pinxhuang@gmail.com>
2019-11-18 20:31:47 -0500

    Create spawnproto.ml

---
+++ spawnproto.ml
@@ -0,0 +1,34 @@
+open Objects
+open Projectile
+open Gui
+open Sprite
+open Stage
+
+let rec game_loop () = 
+  Unix.sleepf 0.05;
+  spawn_enemy ();
+  update_pos player.image;
+  move_enemies !enemy_list;
+  move_projectiles !lasers_list;
+  Graphics.clear_graph ();
+  draw player.image;
+  draw_enemies !enemy_list;
+  draw_list !lasers_list;
+  cleanup_enemies (); 
+  cleanup_lasers ();
+  print_st (string_of_float !spawn_timer);
+  print_st ("number of enemies: " ^ string_of_int (List.length !enemy_list));
+  game_loop ()
+
+let main () = 
+  open_game_window gui_window;
+  draw player.image;
+  player.image.img <- Some (create_image player.image.name);
+  (* enemy.image.img <- Some (create_image enemy.image.name); *)
+  set_image_dimensions player.image;
+  game_loop ()
+
+let test () = 
+  Graphics.open_graph " 640x480"
+
+let () = main ()
