e79f65eaa12d371e875d6443ade09508026606d1
Pinxuan Huang <pinxhuang@gmail.com>
2019-11-19 22:52:25 -0500

    Create demo file

--- spawnproto.ml
+++ spawnproto.ml
@@ -1,43 +1,43 @@
-open Objects
-open Projectile
-open Gui
-open Sprite
-open Stage
+(* open Objects
+   open Projectile
+   open Gui
+   open Sprite
+   open Stage
 
-let rec game_loop () = 
-  Unix.sleepf 0.05;
+   let rec game_loop () = 
+   Unix.sleepf 0.05;
 
-  spawn_enemy ();
+   spawn_enemy ();
 
-  update_pos player.image;
-  shoot_laser player.image;
+   update_pos player.image;
+   shoot_laser player.image;
 
-  move_enemies !enemy_list;
-  move_projectiles !lasers_list;
+   move_enemies !enemy_list;
+   move_projectiles !lasers_list;
 
-  Graphics.clear_graph ();
+   Graphics.clear_graph ();
 
-  draw player.image;
-  draw_enemies !enemy_list;
-  draw_list !lasers_list;
+   draw player.image;
+   draw_enemies !enemy_list;
+   draw_list !lasers_list;
 
-  cleanup_enemies (); 
-  cleanup_lasers ();
+   cleanup_enemies (); 
+   cleanup_lasers ();
 
-  print_st (string_of_float !spawn_timer);
-  print_st ("number of enemies: " ^ string_of_int (List.length !enemy_list));
+   print_st (string_of_float !spawn_timer);
+   print_st ("number of enemies: " ^ string_of_int (List.length !enemy_list));
 
-  game_loop ()
+   game_loop ()
 
-let main () = 
-  open_game_window gui_window;
-  draw player.image;
-  player.image.img <- Some (create_image player.image.name);
-  (* enemy.image.img <- Some (create_image enemy.image.name); *)
-  set_image_dimensions player.image;
-  game_loop ()
+   let main () = 
+   open_game_window gui_window;
+   draw player.image;
+   player.image.img <- Some (create_image player.image.name);
+   (* enemy.image.img <- Some (create_image enemy.image.name); *)
+   set_image_dimensions player.image;
+   game_loop ()
 
-let test () = 
-  Graphics.open_graph " 640x480"
+   let test () = 
+   Graphics.open_graph " 640x480"
 
-let () = main ()
+   let () = main () *)
