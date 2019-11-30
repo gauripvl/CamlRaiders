e66bd68064cb025cf42376a55cfe05f9dd56b2cd
Pinxuan Huang <pinxhuang@gmail.com>
2019-11-29 19:58:16 -0500

    Add is_btn function to Utils

--- utils.ml
+++ utils.ml
@@ -13,6 +13,9 @@ let random_int bound =
   Random.self_init ();
   Random.int bound
 
+let is_btn min max value = 
+  value >= min && value <= max 
+
 let is_onscreen s = 
   s.x > 0 && s.x < gui_window.width &&
   s.y > 0 && s.y < gui_window.height 
--- utils.mli
+++ utils.mli
@@ -6,8 +6,13 @@ val timer : ('a -> unit) -> 'a -> float ref -> float -> unit
 (** [switch_duration s r t] *)
 val switch_duration : bool ref -> float ref-> float ->unit
 
-(** [random_int n] is a random int between 0 (inclusive) and [n] (exclusive). *)
+(** [random_int n] is a random int between 0 (inclusive) 
+    and [n] (exclusive). *)
 val random_int : int -> int
+
+(** [is_btn min max n] is true if [n] is between [min] (inclusive) 
+    and [max] (inclusive). *)
+val is_btn : 'a -> 'a -> 'a -> bool
 
 (** [is_onscreen s] is [true] if the current x, y coordinates 
     of [s] is a point on the gui *)
