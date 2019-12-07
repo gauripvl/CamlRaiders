49c8e2edf0f3634b8b8c01aa707254a4ffe0b7c0
Pinxuan Huang <pinxhuang@gmail.com>
2019-12-07 15:17:04 -0500

    Revise Treasure module

--- treasure.ml
+++ treasure.ml
@@ -5,15 +5,15 @@ type type_treasure = {
   image : sprite
 }
 
-let create_treasure (enemy_s) = {
+let create_treasure (e:Enemy.type_enemy) = {
   image = {
     img = Some (create_image "serpent");
     name = "treasure1";
     height = -1;
     width = -1;
     speed = 3; 
-    x = enemy_s.x; 
-    y = enemy_s.y + 5; 
+    x = e.image.x; 
+    y = e.image.y + 5; 
   }
 }
 
@@ -32,5 +32,5 @@ let rec move_treasure = function
   | [] -> () 
   | h::t -> h.image.y <- h.image.y - h.image.speed; move_treasure t
 
-let cleanup_enemies () = 
-  List.filter (fun e -> e.image.y > 0) treasure_list
+let cleanup_treasures () = 
+  List.filter (fun x -> x.image.y > 0) treasure_list
