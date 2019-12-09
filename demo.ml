5fb3a42fbd256978adae25d11e71a41789bf5b2c
Pinxuan Huang <pinxhuang@gmail.com>
2019-12-09 09:26:46 -0500

    Extract code for initializing main game

--- demo.ml
+++ demo.ml
@@ -3,16 +3,18 @@ open Gui
 open Sprite
 open Stage 
 
-let main () = 
-  open_game_window gui_window;
-  draw player.image;
+let initialize_sprites () = 
   player.image.img <- Some (create_image player.image.name);
+  set_image_dimensions player.image;
+
   Boss.boss_rbbinary.image.img <- Some (
       create_image Boss.boss_rbbinary.image.name);
-  set_image_dimensions player.image;
-  set_image_dimensions Boss.boss_rbbinary.image; 
+  set_image_dimensions Boss.boss_rbbinary.image
+
+let main () = 
+  open_game_window gui_window;
+  initialize_sprites ();
   loop_game ();
-  (* boss_dialogue (); *)
   Graphics.clear_graph (); 
   draw_game_over_screen ()
 
